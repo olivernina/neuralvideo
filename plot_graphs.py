fdf3667739754f5c815feb7d354f43ff8383cfe2
oliver <olivernina@gmail.com>
2015-10-28 23:41:57 -0400

    icoupled configuration

--- plot_graphs.py
+++ plot_graphs.py
@@ -141,6 +141,49 @@ def val_plot(argv):
     # pyplot.show()
 
 
+def bleu_plot(argv):
+    colors = ['#FC474C','#8DE047','#FFDD50','#53A3D7']
+    max_x = 0
+    max_y = 0
+
+    column_num = 6
+    for i,filename in enumerate(argv):
+        file_path = os.path.join(work_dir,filename)
+        if os.path.exists(file_path):
+            data = genfromtxt(file_path,delimiter=',')
+
+            init_val = 0
+            if i==0:
+                fig = pyplot.figure(figsize=(6, 6))
+                axes = pyplot.gca()
+                pyplot.grid()
+
+                max_y = data[init_val:,column_num].max()
+                max_x = data[init_val:,0].max()
+
+                axes.set_ylim([0, max_y])
+                axes.set_xlim([0, max_x])
+                pyplot.xlabel('Iter')
+                pyplot.ylabel('BLEU')
+                pyplot.title('')
+
+            if data[init_val:,column_num].max() > max_y:
+                max_y = data[init_val:,column_num].max()
+                axes.set_ylim([0, max_y])
+
+            if data[init_val:,0].max() > max_x:
+                max_x = data[init_val:,0].max()
+                axes.set_xlim([0, max_x])
+
+            pyplot.plot(data[init_val:,0], data[init_val:,column_num], linewidth=2, label=filename, color=colors[i])
+        else:
+            print "file: "+work_dir+filename+" not found"
+
+    pyplot.legend(loc='upper left', shadow=True, fontsize='medium')
+    pyplot.savefig(os.path.join(work_dir,'val.png'))
+    # pyplot.show()
+
+
 if __name__=="__main__":
     work_dir = sys.argv[1]
     plot_type = sys.argv[2]
@@ -151,3 +194,5 @@ if __name__=="__main__":
         ppl2_plot(sys.argv[3:])
     if plot_type == 'val':
         val_plot(sys.argv[3:])
+    if plot_type == 'bleu':
+        bleu_plot(sys.argv[3:])
